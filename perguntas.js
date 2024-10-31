39118babac4147bce4a3df794eb1fb620f3a559a
leandrodovaleneves <valenevesleandro780@gmail.com>
2024-10-30 21:58:08 -0300

    30/10/2024

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Programação',
+    'O que é Python?',
+    'O Python é uma linguagem de programação'
+)
+
+criaCartao(
+    'Programação',
+    'O que é C++?',
+    'C++ é desenvolvido para ser uma linguagem tipada estaticamente e de propósito geral que é tão eficiente e portátil quanto o C'
+)
+
+criaCartao(
+    'Programação',
+    'O que é uma função?',
+    'Uma função é um bloco de código que executa alguma tarefa'
+)
+
+criaCartao(
+    'Programação',
+    'O que é Ruby?',
+    'é uma linguagem de programação altamente flexível'
+) 
